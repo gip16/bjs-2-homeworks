5bddd3f3d0b51472bb754d25b459b106f8d005e0
“Aleksandr <gipers16@gmail.com>
2022-12-16 20:49:03 +0300

    Homework 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false
+  }
+  Number(percent, contribution, amount, countMonths);
+  let monthlyPercent = percent / 100 / countMonths;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1)));
+  let fullPayment = monthlyPayment * countMonths;
+  return fullPayment.toFixed(2);
 }
