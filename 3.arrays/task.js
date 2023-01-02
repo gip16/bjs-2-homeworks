571ea3a147069af94f3914e9fa70d578f7c0e001
“Aleksandr <gipers16@gmail.com>
2023-01-02 21:20:31 +0300

    Exercise 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+    if (arr1.every((item, index) => item === arr2[index])){
+      return true;
+    }
+  }
+  return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender).reduce((acc, user, index, arr) => {
+    acc += user.age;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
 }
