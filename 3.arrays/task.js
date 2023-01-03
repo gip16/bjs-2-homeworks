abe0fbefcceafb2bac762825b7a06f4335fce4f4
“Aleksandr <gipers16@gmail.com>
2023-01-03 11:34:04 +0300

    Exercise 3 fix

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,5 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    if (arr1.every((item, index) => item === arr2[index])){
-      return true;
-    }
-  }
-  return false;
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
