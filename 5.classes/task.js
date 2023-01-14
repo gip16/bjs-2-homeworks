9181ca99e60f76bfea38cf9ef109ca5b0431eaae
“Aleksandr <gipers16@gmail.com>
2023-01-14 16:45:27 +0300

    Exercise 5 new variation

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -82,20 +82,34 @@ class Library {
   }
 
   findBookBy(type, value) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i][type] === value) {
-        return this.books[i];
-      }
+    const found = this.books.find(book => book[type] === value);
+    if (found) {
+      return found;
     }
+
+    // for (let i = 0; i < this.books.length; i++) {
+    //   if (this.books[i][type] === value) {
+    //     return this.books[i];
+    //   }
+    // }
+    
     return null;
   }
 
   giveBookByName(bookName) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].name === bookName) {
-        return this.books.splice(i, 1)[0];
-      }
+    const bookToGive = this.findBookBy('name', bookName);
+    if (bookToGive) {
+      const index = this.books.indexOf(bookToGive);
+      this.books.splice(index, 1);
+      return bookToGive;
     }
+
+    // for (let i = 0; i < this.books.length; i++) {
+    //   if (this.books[i].name === bookName) {
+    //     return this.books.splice(i, 1)[0];
+    //   }
+    // }
+
     return null;
   }
 }
