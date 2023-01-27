ca0eed1640d813a298e0e467efff1cca7e1ad816
“Aleksandr <gipers16@gmail.com>
2023-01-27 22:07:51 +0300

    Exercise 7

---
+++ 7.async/task.js
@@ -0,0 +1,60 @@
+"use strict"
+
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+    if (this.alarmCollection.some(item => item.time === time)) {
+      throw console.warn('Уже присутствует звонок на это же время');
+    }
+    this.alarmCollection.push({
+      callback,
+      time,
+      canCall: true
+    })
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    const date = new Date();
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+    const alarmCheck = () => this.alarmCollection.forEach(item => {
+      if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+        item.canCall = false;
+        item.callback();
+      }
+    });
+    
+    console.log(alarmCheck);
+    this.intervalId = setInterval(alarmCheck, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(item => item.canCall = true);
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
 //Задача № 1
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  return function(...args) {
+    const hash = md5(args);
+    const cacheObject = cache.find(obj => obj.hash === hash);
+    if (cacheObject) {
+      return `Из кэша: ${cacheObject.value}`;
+    }
+    const result = func(...args);
+    cache.push({hash, value: result});
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    return `Вычисляем: ${result}`;
+  }
 }
 
 //Задача № 2
+
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (timeoutId === null) {
+      wrapper.count++;
+      return func(...args);
+    }
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      timeoutId = null;
+      wrapper.count++;
+      return func(...args);
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
